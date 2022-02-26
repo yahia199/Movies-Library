d8bda71b4c8bf16c028049d00ec6fbe6343759ce
yahia199 <yahiakhlil0151400@gmail.com>
2022-02-27 01:49:31 +0200

    secound edit

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const APIKEY = process.env.APIKEY;
 const DATABASE_URL = process.env.DATABASE_URL;
 console.log(APIKEY);
 
-const client = new pg.Client(DATABASE_URL);
+// const client = new pg.Client(DATABASE_URL);
 
-// const client = new pg.Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false },
-// });
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 function needData(title, poster_path, overview) {
   this.title = title;
